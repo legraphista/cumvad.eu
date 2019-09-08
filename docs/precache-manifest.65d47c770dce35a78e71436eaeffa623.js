6d8133821c742c6470420dbe230c343d3a1b450f
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2019-09-08 17:25:49 +0300

    add: ./docs for gh pages

---
+++ docs/precache-manifest.65d47c770dce35a78e71436eaeffa623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90126595547d08cec271712e7b2d6ad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22a74f97445314557b5",
+    "url": "/static/css/main.4e32aa64.chunk.css"
+  },
+  {
+    "revision": "8955316ad48bb32f26c2",
+    "url": "/static/js/2.7e8c0348.chunk.js"
+  },
+  {
+    "revision": "d22a74f97445314557b5",
+    "url": "/static/js/main.f0051d1a.chunk.js"
+  },
+  {
+    "revision": "3692ed483759a5fa0ecc",
+    "url": "/static/js/runtime~main.4c1b4f45.js"
+  },
+  {
+    "revision": "062caee1725aa94efb1bbed7ee00db81",
+    "url": "/static/media/angles.062caee1.png"
+  },
+  {
+    "revision": "c5657b9fb1ae0b213ecc59427e3d4dbd",
+    "url": "/static/media/panel.c5657b9f.png"
+  }
+]);
